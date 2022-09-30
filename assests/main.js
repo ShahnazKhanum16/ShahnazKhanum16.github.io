acdd61d2fed067fc0db8a6fcd91aa4f7c75b476b
ShahnazKhanum16 <85401790+ShahnazKhanum16@users.noreply.github.com>
2022-09-30 17:50:37 +0530

    start

--- assests/main.js
+++ assests/main.js
@@ -47,16 +47,16 @@ skillsHeader.forEach((el) =>{
 })
 /*=============qualification tabs===========*/
 const tabs= document.querySelectorAll('[data-target]'),
-    tabsContents=document.querySelectorAll('[data-content]')
+    tabContents=document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab =>{
     tab.addEventListener('click', ()=>{
         const target = document.querySelector(tab.dataset.target)
-        tabsContents.forEach(tabContent=>{
+        tabContents.forEach(tabContent=>{
             tabContent.classList.remove('qualification__active')
         })
         target.classList.add('qualification__active')
-        tab.forEach(tab =>{
+        tabs.forEach(tab =>{
             tab.classList.remove('qualification__active')
         })
         tab.classList.add('qualification__active')
