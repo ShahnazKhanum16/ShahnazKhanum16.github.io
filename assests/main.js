5effb5f750b07dd2db9b57542d7803e349092bc3
ShahnazKhanum16 <85401790+ShahnazKhanum16@users.noreply.github.com>
2022-09-30 19:17:39 +0530

    start

--- assests/main.js
+++ assests/main.js
@@ -52,11 +52,12 @@ const tabs= document.querySelectorAll('[data-target]'),
 tabs.forEach(tab =>{
     tab.addEventListener('click', ()=>{
         const target = document.querySelector(tab.dataset.target)
-        
+
         tabContents.forEach(tabContent=>{
             tabContent.classList.remove('qualification__active')
         })
         target.classList.add('qualification__active')
+        
         tabs.forEach(tab =>{
             tab.classList.remove('qualification__active')
         })
