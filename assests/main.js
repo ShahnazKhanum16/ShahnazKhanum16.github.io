2829533d54a79f54643f53ac834a2175cb2ba03e
ShahnazKhanum16 <85401790+ShahnazKhanum16@users.noreply.github.com>
2022-10-12 21:22:07 +0530

    start

--- assests/main.js
+++ assests/main.js
@@ -86,7 +86,20 @@ modalCloses.forEach((modalClose)=> {
 })
 
 /*===========portfolio swiper==============*/
-
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop:true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 /*===================testimonial=============*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
